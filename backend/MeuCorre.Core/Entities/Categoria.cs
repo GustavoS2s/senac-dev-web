71256a36de6b2923f2151ed03f01193e3155820b
GustavoS2 <batistagustavo28765432@gmail.com>
2025-08-25 21:56:38 -0300

    feat: adiciona entidade de usuario

---
+++ backend/MeuCorre.Core/Entities/Categoria.cs
@@ -0,0 +1,29 @@
+﻿using MeuCorre.Core.Enuns;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeuCorre.Core.Entities
+{
+    public class Categoria : Entidade
+    {
+        public string Nome { get; private set; }
+        public string Descricao { get; private set; }
+        public Tipotransacao tipo  { get; private set; }
+        public string Cor { get; private set; }
+        public string Icone { get; private set; }
+        public DateTime DataCriacao { get; private set; }
+        public bool Ativo { get; private set; }
+        public Categoria(string nome, string descricao, string cor, string icone, DateTime datacriacao, bool ativo)
+        {
+            Nome = nome;
+            Descricao = descricao;
+            Cor = cor;
+            Icone = icone;
+            Ativo = ativo;
+            DataCriacao = datacriacao;
+        }
+    }
+}
