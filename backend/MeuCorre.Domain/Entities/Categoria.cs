54ec2e720459c0b8a871331fe5e5baaa268765e4
GustavoS2 <batistagustavo28765432@gmail.com>
2025-09-15 21:54:22 -0300

    feat(backend) #11 adiciona entidade de categoria

--- backend/MeuCorre.Domain/Entities/Categoria.cs
+++ backend/MeuCorre.Domain/Entities/Categoria.cs
@@ -11,23 +11,46 @@ namespace MeuCorre.Domain.Entities
     public class Categoria : Entidade
     {
         public string Nome { get; private set; }
-        public string Descricao { get; private set; }
+        public string? Descricao { get; private set; }
         public Tipotransacao Tipo  { get; private set; }
-        public string Cor { get; private set; }
-        public string Icone { get; private set; }
+        public string? Cor { get; private set; }
+        public string? Icone { get; private set; }
         public Guid? UsuarioId { get; private set; }
         public bool Ativo { get; private set; }
         public Categoria() { }
 
         public virtual Usuario Usuario { get; private set; }
-        public Categoria(string nome, string descricao, string cor, string icone, Guid usuarioid, bool ativo)
+        public Categoria(string nome,Tipotransacao tipo, bool ativo, string? descricao, string? cor, string? icone, Guid? usuarioid)
         {
             Nome = nome;
             Descricao = descricao;
             Cor = cor;
             Icone = icone;
             UsuarioId = usuarioid;
+            Ativo = true;
+            Tipo = tipo;
+        }
+
+        public void AtualizarInformacoes(string nome,Tipotransacao tipo, bool ativo, string? descricao, string? cor, string? icone)
+        {
+            Nome = nome.ToUpper();
+            Descricao = descricao;
+            Cor = cor;
+            Icone = icone;
+            Tipo = tipo;
             Ativo = ativo;
+            AtualizarDataModificacao();
+        }
+        public void Ativar()
+        {
+            Ativo = true;
+            AtualizarDataModificacao();
+        }
+
+        public void Inativar()
+        {
+            Ativo = false;
+            AtualizarDataModificacao();
         }
     }
 }
