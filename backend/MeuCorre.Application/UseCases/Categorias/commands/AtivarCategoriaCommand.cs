ed8b4faa326c73d5ad8f355574e41f8fbb2efc7f
GustavoS2 <batistagustavo28765432@gmail.com>
2025-09-18 21:47:44 -0300

    feat(backend): adiciona listar todas categorias

---
+++ backend/MeuCorre.Application/UseCases/Categorias/commands/AtivarCategoriaCommand.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using MeuCorre.Domain.interfaces.Repositories;
+using System.ComponentModel.DataAnnotations;
+
+namespace MeuCorre.Application.UseCases.Categorias.commands
+{
+    public class AtivarCategoriaCommand : IRequest<(string, bool)>
+    {
+        [Required(ErrorMessage = "É necessário informar o Id da categoria")]
+        public required Guid CategoriaId { get; set; }
+    }
+
+    internal class AtivarCategoriaCommandHandler : IRequestHandler<AtivarCategoriaCommand, (string, bool)>
+    {
+        private readonly ICategoriaRepository _categoriaRepository;
+        public AtivarCategoriaCommandHandler(ICategoriaRepository categoriaRepository)
+        {
+            _categoriaRepository = categoriaRepository;
+        }
+        public async Task<(string, bool)> Handle(AtivarCategoriaCommand request, CancellationToken cancellationToken)
+        {
+            var categoria = await _categoriaRepository.ObterPorIdAsync(request.CategoriaId);
+
+            categoria.Ativar();
+
+            await _categoriaRepository.AtualizarAsync(categoria);
+            return ("Categoria ativada com sucesso.", true);
+        }
+    }
+}
+
---
+++ backend/MeuCorre.Application/UseCases/Categorias/commands/InativarCategoriaCommand.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using MeuCorre.Domain.interfaces.Repositories;
+using System.ComponentModel.DataAnnotations;
+
+namespace MeuCorre.Application.UseCases.Categorias.commands
+{
+    public class InativarCategoriaCommand : IRequest<(string, bool)>
+    {
+        [Required(ErrorMessage = "É necessário informar o Id da categoria")]
+        public required Guid CategoriaId { get; set; }
+    }
+
+    internal class InativarCategoriaCommandHandler : IRequestHandler<InativarCategoriaCommand, (string, bool)>
+    {
+        private readonly ICategoriaRepository _categoriaRepository;
+        public InativarCategoriaCommandHandler(ICategoriaRepository categoriaRepository)
+        {
+            _categoriaRepository = categoriaRepository;
+        }
+        public async Task<(string, bool)> Handle(InativarCategoriaCommand request, CancellationToken cancellationToken)
+        {
+            var categoria = await _categoriaRepository.ObterPorIdAsync(request.CategoriaId);
+
+            categoria.Ativar();
+
+            await _categoriaRepository.AtualizarAsync(categoria);
+            return ("Categoria desativada com sucesso.", true);
+        }
+    }
+}
