6bbf2657afd13c7094b824414e93dda0756feaef
GustavoS2 <batistagustavo28765432@gmail.com>
2025-09-29 21:29:22 -0300

    feat(backend): Adicionar estrutura de contas e enums de tipo de conta

---
+++ backend/MeuCorre.Domain/Enuns/TipoConta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeuCorre.Domain.Enuns
+{
+    public enum TipoConta
+    {
+        Carteira = 1,
+        ContaBancaria = 2,
+        CartaoCredito = 3
+    }
+}
