f8e229d683cfd435ea6b6ebc659a2356e8e326f7
GustavoS2s <batistagustavo28765432@gmail.com>
2025-11-05 21:25:14 -0300

    feat: atualiza categoria

--- frontend/MeuCorre/src/app/pages/categoria/categoria.ts
+++ frontend/MeuCorre/src/app/pages/categoria/categoria.ts
@@ -9,4 +9,22 @@ import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 })
 export class Categoria {
   active = 1;
+
+  categoria_receitas = [
+    { nome: 'Café da Manhã', imagem: 'assets/categorias/cafe-manha.jpg' },
+    { nome: 'Almoço', imagem: 'assets/categorias/almoco.jpg' },
+    { nome: 'Lanches', imagem: 'assets/categorias/lanches.jpg' },
+    { nome: 'Jantar', imagem: 'assets/categorias/jantar.jpg' },
+    { nome: 'Sobremesas', imagem: 'assets/categorias/sobremesas.jpg' },
+    { nome: 'Bebidas', imagem: 'assets/categorias/bebidas.jpg' },
+  ];
+
+  categoria_despesas = [
+    { nome: 'Moradia', icone: 'assets/categorias/moradia.jpg' },
+    { nome: 'Alimentação', imagem: 'assets/categorias/alimentacao.jpg' },
+    { nome: 'Transporte', imagem: 'assets/categorias/transporte.jpg' },
+    { nome: 'Saúde', imagem: 'assets/categorias/saude.jpg' },
+    { nome: 'Lazer', imagem: 'assets/categorias/lazer.jpg' },
+    { nome: 'Educação', imagem: 'assets/categorias/educacao.jpg' },
+  ];
 }
